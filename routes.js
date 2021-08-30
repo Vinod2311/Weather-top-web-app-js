0d7c622d7f2adec961894570292f1d65d424517b
Glitch (weather-top-js) <none>
2021-08-30 07:55:08 +0000

    🚓🚩 Checkpoint ./routes.js:35366017/1119

--- routes.js
+++ routes.js
@@ -3,12 +3,26 @@
 const express = require("express");
 const router = express.Router();
 
+const start = require("./controllers/start.js")
+const accounts = require("./controllers/accounts.js")
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
+const stationCtrl = require("./controllers/stationCtrl.js");
 
-router.get("/", dashboard.index);
+
+router.get("/", start.index);
+router.get("/signup", accounts.signup);
+router.get("/login", accounts.login);
+router.post("/register", accounts.register);
+router.post("/authenticate", accounts.authenticate);
+router.post("/member/editMemberDetails", accounts.editMemberDetails);
+router.get("/logout", accounts.logout);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
-router.get("")
+router.get("/stations/:id", stationCtrl.index);
+router.get("/dashboard/deleteStation/:id", dashboard.deleteStation);
+router.get("/stations/:id/deleteReading/:readingId", stationCtrl.deleteReading);
+router.get("/stations/:id/addReading", stationCtrl.addReading);
+router.get("/dashboard/addStation", dashboard.addStation)
 
 module.exports = router;
