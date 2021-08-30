1cb4cf7e57028818f46235918d5fa50dc88cd56f
Glitch (weather-top-js) <none>
2021-08-30 11:25:10 +0000

    🎐📖 Checkpoint ./controllers/dashboard.js:35366017/227

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,15 +2,17 @@
 
 const logger = require("../utils/logger");
 const stationCollection = require('../models/readings-store.js');
+const accounts = require("accounts.js")
+const userStore = require("../models/user-store.js");
 
 const dashboard = {
   index(request, response) {
-    
+    const loggedInUser = accounts.getCurrentUser(request);
     const viewData = {
       title: "Dashboard",
-      stations: stationCollection,
+      stations: userStore.getUser,
     };
-    logger.info("dashboard rendering",stationCollection.name);
+    logger.info("dashboard rendering",stationCollection);
     response.render("dashboard", viewData);
   },
 };
