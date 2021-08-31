1c91c4abcd454879eda177529d3d81a496e82c53
Glitch (weather-top-js) <none>
2021-08-31 23:40:37 +0000

    🌄🍠 Checkpoint ./routes.js:35366017/147 ./views/partials/listReading.hbs:35366017/10

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ const accounts = require("./controllers/accounts.js")
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 const stationCtrl = require("./controllers/stationCtrl.js");
-
+const stationStore = require("./models/station-store.js");
 
 router.get("/", start.index);
 router.get("/signup", accounts.signup);
@@ -19,9 +19,9 @@ router.post("/authenticate", accounts.authenticate);
 router.get("/logout", accounts.logout);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
-router.get("/stations/:id", stationCtrl.index);
+//router.get("/stations/:id", stationCtrl.index);
 router.get("/dashboard/deleteStation/:id", dashboard.deleteStation);
-//router.get("/stations/:id/deleteReading/:readingId", stationCtrl.deleteReading);
+router.get("/stations/:id/deleteReading/:readingId", stationStore.removeReading);
 //router.post("/stations/:id/addReading", stationCtrl.addReading);
 router.post("/dashboard/addStation", dashboard.addStation)
 
